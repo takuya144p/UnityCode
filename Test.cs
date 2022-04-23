c078d742bde0e2e60269ced4add409503a11828f
takuya144p <103823257+takuya144p@users.noreply.github.com>
2022-04-23 10:35:09 +0800

    Create Test.cs
    test

---
+++ Test.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+
+    Slider hpSlider;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        hpSlider = GetComponent<Slider>();
+
+        float maxHp = 200f;
+        float nowHp = 100f;
+
+
+        //スライダーの最大値の設定
+        hpSlider.maxValue = maxHp;
+
+        //スライダーの現在値の設定
+        hpSlider.value = nowHp;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+}
